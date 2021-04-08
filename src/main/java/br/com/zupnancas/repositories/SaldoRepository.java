bddf2ddaa3dc3edb2105962a58abb76548ea3b2a
Ana Luiza Santos <71670666+uaianaluiza@users.noreply.github.com>
2021-04-08 15:47:21 -0300

    criar SaldoRepository

---
+++ src/main/java/br/com/zupnancas/repositories/SaldoRepository.java
@@ -0,0 +1,7 @@
+package br.com.zupnancas.repositories;
+
+import br.com.zupnancas.models.Saldo;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SaldoRepository extends CrudRepository<Saldo,String> {
+}
