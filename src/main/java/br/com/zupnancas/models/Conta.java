b55e91733753cc95064e91032e22ee732d6219df
Ana Luiza Santos <71670666+uaianaluiza@users.noreply.github.com>
2021-04-08 15:03:05 -0300

    criar Conta model

--- src/main/java/br/com/zupnancas/models/Conta.java
+++ src/main/java/br/com/zupnancas/models/Conta.java
@@ -1,4 +1,69 @@
 package br.com.zupnancas.models;
 
+import br.com.zupnancas.enums.Status;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.time.LocalDate;
+@Entity
+@Table(name = "contas")
 public class Conta {
+    private int id;
+    private Double valor;
+    private String descricao;
+    private LocalDate dataDeSaida;
+    private LocalDate dataVencimento;
+    private Status statusEnum;
+
+    public Conta(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getDataDeSaida() {
+        return dataDeSaida;
+    }
+
+    public void setDataDeSaida(LocalDate dataDeSaida) {
+        this.dataDeSaida = dataDeSaida;
+    }
+
+    public LocalDate getDataVencimento() {
+        return dataVencimento;
+    }
+
+    public void setDataVencimento(LocalDate dataVencimento) {
+        this.dataVencimento = dataVencimento;
+    }
+
+    public Status getStatusEnum() {
+        return statusEnum;
+    }
+
+    public void setStatusEnum(Status statusEnum) {
+        this.statusEnum = statusEnum;
+    }
 }
