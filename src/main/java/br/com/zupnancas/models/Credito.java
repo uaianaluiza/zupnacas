6bb51aaa6e9c634eb53ec8648a39dec658e587ed
Ana Luiza Santos <71670666+uaianaluiza@users.noreply.github.com>
2021-04-08 14:27:54 -0300

    criar credito model

--- src/main/java/br/com/zupnancas/models/Credito.java
+++ src/main/java/br/com/zupnancas/models/Credito.java
@@ -1,4 +1,49 @@
 package br.com.zupnancas.models;
 
+import org.springframework.cglib.core.Local;
+
+import javax.persistence.Id;
+
 public class Credito {
+    @Id
+    private int id;
+    private double valor;
+    private String descricao;
+    private Local DataDeEntrada;
+
+    public Credito(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Local getDataDeEntrada() {
+        return DataDeEntrada;
+    }
+
+    public void setDataDeEntrada(Local dataDeEntrada) {
+        DataDeEntrada = dataDeEntrada;
+    }
 }
