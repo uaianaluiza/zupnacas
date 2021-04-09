11bf1122a547d9868b7609a8cd1c07c7dad52991
Ana Luiza Santos <71670666+uaianaluiza@users.noreply.github.com>
2021-04-09 15:17:50 -0300

    Criar Crédito Service

--- src/main/java/br/com/zupnancas/services/CreditoService.java
+++ src/main/java/br/com/zupnancas/services/CreditoService.java
@@ -1,6 +1,7 @@
 package br.com.zupnancas.services;
 
 import br.com.zupnancas.models.Credito;
+import br.com.zupnancas.repositories.CategoriaRepository;
 import br.com.zupnancas.repositories.CreditoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,12 +10,22 @@ import java.time.LocalDate;
 
 @Service
 public class CreditoService {
+
     @Autowired
     CreditoRepository creditoRepository;
+    CategoriaService categoriaService;
 
-    public Credito adicionarCredito(Credito credito){
+    public CreditoService (CreditoRepository creditoRepository, CategoriaService categoriaService){
+        this.creditoRepository = creditoRepository;
+        this.categoriaService = categoriaService;
+    }
+
+    public Credito cadastrarCredito (Credito credito){
         credito.setDataDeEntrada(LocalDate.now());
-        Credito novoCredito = creditoRepository.save(credito);
-        return novoCredito;
+        return creditoRepository.save(credito);
+    }
+
+    public Iterable<Credito> verTodosOsCreditos(){
+        return creditoRepository.findAll();
     }
 }
